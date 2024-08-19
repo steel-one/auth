ea2361090f3dcf329943f235a6b3e704c8d17bb6
ViacheslavDenisenko <vdenisenko@centrak.com>
2024-08-19 20:59:48 +0400

    caching with cache-manager

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,3 +1,4 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
@@ -6,5 +7,6 @@ import { UserService } from './user.service';
     providers: [UserService],
     controllers: [UserController],
     exports: [UserService],
+    imports: [CacheModule.register()],
 })
 export class UserModule {}
